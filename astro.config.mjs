c7f0141cf59a597d1dd202ec1e930dde254de104
叶桐 <2304711231@qq.com>
2025-01-20 17:53:05 +0800

    远程部署
    远程部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,8 @@ const envAdapter = () => {
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astronaut.github.io',
+  base: 'high-eq',
   integrations: [
     unocss(),
     solidJs(),
